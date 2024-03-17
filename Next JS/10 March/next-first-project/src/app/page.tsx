a3758a7eb57f11f897a9f2f448384d270fac8027
Ahmad Jamal <ahmadjamal6886@gmail.com>
2024-03-17 16:37:20 +0500

    Next Project Assignment

--- Next JS/10 March/next-first-project/src/app/page.tsx
+++ Next JS/10 March/next-first-project/src/app/page.tsx
@@ -2,9 +2,19 @@ export default function Home() {
   return (
         <div>Hello World
           <h1>
-            PIAIC Batch-52
+            PIAIC Batch 52
           </h1>
+
+          <div>
+
+Ahmad Jamal
+<h4>
+SIR JAHANZAIB ASSIGNMENT
+
+</h4>
+          </div>
         </div>
-        
+
+       
   );
 }
